1292e4f9856533650044f48b8fc82d5c6cb5c181
anand2312 <anandkrishna2312@gmail.com>
2021-11-26 18:10:43 +0400

    Add helper functions for 2021

---
+++ 2021/README.md
@@ -0,0 +1,6 @@
+# 2021!
+
+Doing stuff a little different this year.
+`helper.py` will automatically download the input into `inputs` dir. Just import `Puzzle` from `helpers` and instantiate with the
+day number.
+If anyone else is reading this, this script will need the session cookie set by advent of code to be set as an _environment variable_ `AOC_SESSION_COOKIE`.
---
+++ 2021/helpers.py
@@ -0,0 +1,44 @@
+"""Helper functions"""
+import os
+import pathlib
+from typing import List
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv(pathlib.Path(__file__).parent.parent / ".env")
+
+
+BASE_URL = "https://adventofcode.com/2021/day/{day}"
+INPUT_URL = BASE_URL + "/input"
+INPUT_DIR = pathlib.Path(__file__).parent / "inputs"
+
+
+class Puzzle:
+    def __init__(self, day: int) -> None:
+        self.day = day
+        self._input_file = INPUT_DIR / f"day_{day:0>2}.txt"
+
+        if self._input_file.exists():
+            self.raw_content = self._input_file.read_text()
+        else:
+            self.get_input_data()
+
+    def intlines(self) -> List[int]:
+        """Splits the raw content and converts each element to an integer"""
+        return [int(i) for i in self.raw_content.split()]
+
+    def lines(self) -> List[str]:
+        """Returns the input as a list of lines"""
+        return self.raw_content.split()
+    
+    def get_input_data(self) -> None:
+        """Make an HTTP request to AoC, fetch the input data and make a file."""
+        session = os.environ.get("AOC_SESSION_COOKIE")
+        r = requests.get(INPUT_URL.format(day=self.day), cookies={"session": session})
+        
+        self.raw_content = r.text
+        self._input_file = INPUT_DIR / f"day_{self.day:0>2}.txt"
+        self._input_file.write_text(r.text)
+
+Puzzle(1).get_input_data()
