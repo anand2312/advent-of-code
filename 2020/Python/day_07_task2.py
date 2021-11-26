5e8ee484feb1830688d8cc6f7624f6b2d730965c
anand2312 <anandkrishna2312@gmail.com>
2021-11-26 11:20:09 +0400

    Day 7, Task 2

--- 2020/Python/day_07_task2.py
+++ 2020/Python/day_07_task2.py
@@ -1,1 +1,30 @@
 """Advent of Code Day 7"""
+from typing import Dict
+
+from day_07_task1 import Bag, parse_all_rules
+from utils import get_data
+
+
+raw_data = get_data("bags") # data is now in the form of a list of strings, with each string being a bag rule
+bags = parse_all_rules(raw_data)
+
+# Bag -> name: str, the color of the bag and children: dict[str, int], with color name: count
+# problem is to count the total number of bags inside our shiny gold bag
+
+shiny_gold = bags["shiny gold"]
+
+
+def count_children(bag: Bag, count: int, all_bags: Dict[str, Bag]) -> int:
+    # recursively count the number of child bags in the specified bag
+    if bag.children is None:
+        return count
+    
+    for child_color, child_count in bag.children.items():
+        child_bag = all_bags[child_color]
+        count += child_count
+        count += child_count * count_children(child_bag, 0, all_bags)
+    
+    return count
+
+
+print(count_children(shiny_gold, 0, bags))
