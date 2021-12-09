54521ffcc7c5e983f0d7cc27ce1c981c2dfc6e4f
anand2312 <anandkrishna2312@gmail.com>
2021-12-09 11:21:31 +0400

    Day 9 Task 1

---
+++ 2021/solutions/day_09_task_01.py
@@ -0,0 +1,31 @@
+import numpy as np
+from helpers import Puzzle
+
+
+puzzle = Puzzle(day=9)
+
+
+def parse_input(raw: str) -> np.ndarray:
+    return np.array([list(map(int, line)) for line in raw.splitlines()])
+
+
+def main(raw: str) -> int:
+    lines = parse_input(raw)
+    padded = np.pad(lines, 1, "constant", constant_values=9)
+
+    # make another array that's padded at the top and bottom
+    # then use this to compare each element with it's neighbours
+
+    lows = (
+        (lines < padded[2:, 1:-1])  # shift upwards
+        & (lines < padded[:-2, 1:-1])  # downwards
+        & (lines < padded[1:-1, 2:])  # right
+        & (lines < padded[1:-1, :-2])  # left
+    )
+
+    return (lines[lows] + 1).sum()
+
+
+if __name__ == "__main__":
+    raw = puzzle.raw_content
+    print(main(raw))
